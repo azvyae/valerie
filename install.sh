2ab155c05bd20aff8795f7ec1fb54ba64f68991a
Azvya Erstevan I <erstevn@gmail.com>
2023-01-15 22:38:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-docker run --rm -u "$(id -u):$(id -g)" -v $(pwd):/var/www/html -w /var/www/html laravelsail/php81-composer:latest composer install --ignore-platform-reqs
+docker run --rm -u "$(id -u):$(id -g)" -v $(pwd):/var/www/html -w /var/www/html laravelsail/php81-composer:latest composer install --ignore-platform-reqs 
 
 if [ ! -f ./docker/php/config/opcache.ini ]; then
     cp ./docker/php/config/opcache.ini-example ./docker/php/config/opcache.ini
